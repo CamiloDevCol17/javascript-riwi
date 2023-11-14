393d873cd550eea0291e9af9e8ba34e74236d6cf
DAYRON <zbastian.zapata@gmail.com>
2023-11-14 18:10:15 -0500

    Javascript conceptos basicos añadido

---
+++ conceptos_basicos/conceptos.js
@@ -0,0 +1,27 @@
+// Pre-incremento y Post-incremento
+
+// let x = 1
+// let y = ++x //Preincremento = Se incrementa y de una se agrega ese valor a la variable
+
+// console.log(x);
+// console.log(y);
+
+// let z = x++  // Postincremento = Toma el valor de la variable y luego se incrementa
+
+// console.log(z);
+// console.log(x);
+
+
+// Switch
+
+
+let operacion = 5 + 2
+
+switch (operacion) {
+    case 0:
+        console.log("La operacion es igual a 0");
+        break;
+    case 7:
+        console.log("La operacion es igual a 7");
+        break
+}
--- ejercicios/ejercicio2.js
+++ ejercicios/ejercicio2.js
@@ -1,4 +1,21 @@
 // 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+function alReves(texto="") {
+    reverse = texto.split("").reverse().join("")
+}
+
+
+alReves("Hola como estas")
+
+// alReves("Hola Mundo")
 // 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+function contarPalabraRepetida(texto1 = "", palabraABuscar ="") {
+    search = texto1.search(palabraABuscar)
+
+    console.log(search);
+}
+
+contarPalabraRepetida("hola mundo adios mundo", "mundo")
+
+
 // 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
